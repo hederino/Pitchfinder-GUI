0ce20eee5fd907ea63e2294f7822bb4bb8e1ff72
Hederino <hederino@mail.com>
2023-05-27 22:23:11 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+import sys
+from PyQt6 import QtWidgets, QtCore
+from widgets import Widget, MainWindowSubwidget, ExitButton
+from note import Note, note_a4
+
+
+def lineedit_to_output(s: str):
+    try:
+        freq = float(s)
+        n, cents = Note.freq_to_note(freq)
+        note_str = str(n) if n.is_natural else f"{n}/{n.enharmonic_note()}"
+        return f"{note_str} ({cents:+} cents)"
+    except ValueError:
+        return "Invalid input." 
+ 
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Pitchfinder GUI")
+        # set icon
+        self.setFixedSize(400, 180)
+        self.init_UI()
+        self.set_menubar()
+        self.connect_signals()
+
+    def init_UI(self):    
+
+        self.main_layout = QtWidgets.QVBoxLayout()
+        self.main_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
+
+        self.setCentralWidget(Widget())
+
+        self.main_subwidget = MainWindowSubwidget()
+        self.main_layout.addWidget(self.main_subwidget)
+
+        self.quit_button_layout = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.quit_button_layout)
+
+        self.quit_button = ExitButton()
+
+        self.quit_button_layout.addSpacing(self.width() - 2 * self.quit_button.width() // 2)
+        self.quit_button_layout.addWidget(self.quit_button)
+
+        self.centralWidget().setLayout(self.main_layout)
+
+    def set_menubar(self):
+        pass
+
+    def connect_signals(self):
+        line_edit = self.main_subwidget.line_edit_widget.line_edit
+        display_label = self.main_subwidget.display_label
+        enter_button = self.main_subwidget.line_edit_widget.input_button 
+        enter_button.clicked.connect(lambda: display_label.setText(lineedit_to_output(line_edit.text())))
+
+        self.quit_button.clicked.connect(self.close)
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    mw = MainWindow()
+    mw.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()         
---
+++ widgets.py
@@ -0,0 +1,99 @@
+from PyQt6 import QtWidgets, QtCore, QtGui
+
+class Widget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()  
+
+class MainWindowSubwidget(Widget):
+    def __init__(self):
+        super().__init__()
+        self.vlayout = QtWidgets.QVBoxLayout()
+        self.setLayout(self.vlayout)
+
+        self.vlayout.addWidget(InfoLabel())
+
+        self.line_edit_widget = LineEditWidget()
+        self.vlayout.addSpacing(8)
+        self.vlayout.addWidget(self.line_edit_widget)
+        self.vlayout.addSpacing(16)
+        self.display_label = DisplayLabel()
+        self.vlayout.addWidget(self.display_label)
+        self.vlayout.itemAt(4).setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter)
+
+class Label(QtWidgets.QLabel):
+    def __init__(self):
+        super().__init__()
+        self.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+
+class InfoLabel(Label):
+    def __init__(self):
+        super().__init__()
+        self.setText("Enter a frequency value in Hz to find the closest note:")
+
+
+class DisplayLabel(Label):
+    def __init__(self):
+        super().__init__()
+        self.setFrameShape(QtWidgets.QFrame.Shape.Box)
+        self.setFixedHeight(40)
+        self.setFixedWidth(165)
+        self.setFont(QtGui.QFont("Segoe UI Symbol")) # this font correctly displays the flat symbol 
+
+
+class PushButton(QtWidgets.QPushButton):
+    def __init__(self, text: str = None):
+        super().__init__()
+        self.setFixedHeight(25)
+        self.setFixedWidth(50)
+        self.setText(text)
+ 
+
+class EnterButton(PushButton):
+    def __init__(self):
+        super().__init__(text="Enter")
+
+
+class ExitButton(PushButton):
+    def __init__(self):
+        super().__init__(text="Quit")
+
+
+class InputLineEdit(QtWidgets.QLineEdit):
+    def __init__(self):
+        super().__init__()
+        self.setFixedWidth(150)
+        
+
+class LineEditWidget(Widget):
+    def __init__(self):
+        super().__init__()
+        self.hlayout = QtWidgets.QHBoxLayout()
+        self.setLayout(self.hlayout)
+        self.line_edit = InputLineEdit()
+        self.input_button = EnterButton()
+
+        self.hlayout.addSpacerItem(Spacer())
+        self.hlayout.addWidget(self.line_edit)
+        self.hlayout.addWidget(self.input_button)
+        self.hlayout.addSpacerItem(Spacer())
+        self.hlayout.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+
+        self.hlayout.setSpacing(0)
+        self.hlayout.setContentsMargins(0, 0, 0, 0)
+
+
+class Spacer(QtWidgets.QSpacerItem):
+    def __init__(self):
+        super().__init__(75, 20)
+
+
+class FreqSpinBox(QtWidgets.QDoubleSpinBox):
+    def __init__(self):
+        super().__init__()
+        self.setDecimals(3)
+        self.setMinimum(400.0)  # consts
+        self.setMaximum(500.0)
+        self.setValue(440.0)
+        # self.setFixedWidth(80)
+        self.setSuffix(" Hz")
